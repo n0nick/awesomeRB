7e879f2d5706405dc21abb0a947ab2b016da2da1
Sagie Maoz <sagie@maoz.info>
2011-12-11 21:33:08 +0200

    better use of assertEquals()

--- src/awesomeRB/tests/RBTreeTest.java
+++ src/awesomeRB/tests/RBTreeTest.java
@@ -22,7 +22,7 @@ public class RBTreeTest {
 		tree.insert(3);
 		tree.insert(4);
 		
-		assertEquals(tree.toString(), "<Tree [ 1 x [ 2 x [ 3 x [ 4 x x ] ] ] ]>");
+		assertEquals("<Tree [ 1 x [ 2 x [ 3 x [ 4 x x ] ] ] ]>", tree.toString());
 	}
 	
 	private RBTree createSomeTestTree() {
@@ -37,40 +37,40 @@ public class RBTreeTest {
 	
 	@Test public void insertTest2() {
 		RBTree tree = createSomeTestTree();
-		assertEquals(tree.toString(), "<Tree [ 1 x [ 12 [ 2 x x ] [ 13 x x ] ] ]>");
+		assertEquals("<Tree [ 1 x [ 12 [ 2 x x ] [ 13 x x ] ] ]>", tree.toString());
 	}
 	
 	@Test public void rotateLeftTest1() {
 		RBTree tree = createSomeTestTree();
 		tree.leftRotate(tree.getRoot().getRightChild());
-		assertEquals(tree.toString(), "<Tree [ 1 x [ 13 [ 12 [ 2 x x ] x ] x ] ]>");
+		assertEquals("<Tree [ 1 x [ 13 [ 12 [ 2 x x ] x ] x ] ]>", tree.toString());
 	}
 		
 	@Test public void rotateRightTest1() {
 		RBTree tree = createSomeTestTree();
 		tree.rightRotate(tree.getRoot().getRightChild());
-		assertEquals(tree.toString(), "<Tree [ 1 x [ 2 x [ 12 x [ 13 x x ] ] ] ]>");
+		assertEquals("<Tree [ 1 x [ 2 x [ 12 x [ 13 x x ] ] ] ]>", tree.toString());
 	}
 	
 	@Test public void sizeTest1() {
 		RBTree tree = createSomeTestTree();
-		assertEquals(tree.size(), 4);
+		assertEquals(4, tree.size());
 	}
 	
 	@Test public void insertSizeTest1() {
 		RBTree tree = createSomeTestTree();
 		tree.insert(6);
-		assertEquals(tree.size(), 5);
+		assertEquals(5, tree.size());
 	}
 	
 	@Test public void minTest1() {
 		RBTree tree = createSomeTestTree();
-		assertEquals(tree.min(), 1);
+		assertEquals(1, tree.min());
 	}
 	
 	@Test public void maxTest1() {
 		RBTree tree = createSomeTestTree();
-		assertEquals(tree.min(), 13);
+		assertEquals(13, tree.min());
 	}
 	
 	@Test public void containsTest1() {
@@ -93,14 +93,14 @@ public class RBTreeTest {
 		RBTree tree = createSomeTestTree();
 		tree.delete(13);
 		assert(!tree.contains(13));
-		assertEquals(tree.size(), 3);
-		assertEquals(tree.max(), 12);
+		assertEquals(3, tree.size());
+		assertEquals(12, tree.max());
 	}
 	
 	@Test public void toIntArrayTest1() {
 		RBTree tree = createSomeTestTree();
 		int[] expected = {1, 2, 12, 13};
-		assertArrayEquals(tree.toIntArray(), expected);
+		assertArrayEquals(expected, tree.toIntArray());
 	}
 	
 	@Test public void toIntArrayTest2() {
@@ -112,7 +112,7 @@ public class RBTreeTest {
 		tree.insert(6);
 		
 		int[] expected = {3, 5, 6, 7, 300};
-		assertArrayEquals(tree.toIntArray(), expected);
+		assertArrayEquals(expected, tree.toIntArray());
 	}
 	
 	//TODO isValidTest()
